fc61f98b28d1213d61ec4791c40fc0122c5449aa
Suresh <sureshabde172@gmail.com>
2024-09-03 01:53:54 -0700

    Project

---
+++ reactpro/src/Services.js
@@ -0,0 +1,11 @@
+// import React, { useEffect } from 'react'
+// import { useState } from 'react';
+// function Services(){
+//     const[time,settime]=useState(0);
+//     useEffect(()=>{
+//         settime((time)=>time+1)
+//     },1000);
+// }
+//     return  <div><h2>Th web is launch{time}</h2></div>
+
+// export default Services;
