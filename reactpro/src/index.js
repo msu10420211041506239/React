7b6bc1af3afc77fff41dbf92a970ed1732f61eed
Suresh <sureshabde172@gmail.com>
2024-09-03 09:58:55 -0700

    index

--- reactpro/src/index.js
+++ reactpro/src/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import Api from './Api';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
